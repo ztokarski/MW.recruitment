5bff2869316b97fdf2b74f1629496f489eadabb1
Zbigniew Tokarski <zbigniew.piotr.tokarski@gmail.com>
2017-11-06 20:04:47 +0100

    Create DateRange class with constructor

---
+++ MW.recruitment/DateRange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MW.recruitment
+{
+    public class DateRange
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public DateRange(DateTime startDate, DateTime endDate)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+
+    }
+
+}
