9bbc71d5325e27a1af189c5f9311795c6dc03999
Zbigniew Tokarski <zbigniew.piotr.tokarski@gmail.com>
2017-11-06 16:55:21 +0100

    Create string extension method
    checking if parsing from string to date is possible

---
+++ MW.recruitment/StringExtensionMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MW.recruitment
+{
+    public static class StringExtensionMethods
+    {
+        public static bool TryParseToLocalDate(this string dateString)
+        {
+            try
+            {
+                DateTime.Parse(dateString);
+                return true;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("{0} - This is wrong date format. Your System date format is: {1}", dateString, DateTime.Now.ToShortDateString());
+            }
+            return false;
+        }
+    }
+}
