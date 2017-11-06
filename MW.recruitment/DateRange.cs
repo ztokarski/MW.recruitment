3909c732b44ca75c029058848f5a8797e26435ca
Zbigniew Tokarski <zbigniew.piotr.tokarski@gmail.com>
2017-11-06 20:15:49 +0100

    Add Simple version of DisplayRange Method

--- MW.recruitment/DateRange.cs
+++ MW.recruitment/DateRange.cs
@@ -17,7 +17,19 @@ namespace MW.recruitment
             EndDate = endDate;
         }
 
+        public string DisplayRange(DateRange range)
+        {
+            if (range.StartDate > range.EndDate)
+                return "Error. The StartDate is greater than the EndDate.";
+            else if (range.StartDate == range.EndDate)
+                return range.StartDate.ToShortDateString();
+            else if (range.StartDate.Year == range.EndDate.Year && range.StartDate.Month == range.EndDate.Month)
+                return string.Format("{0} - {1}.{2}.{3}", range.StartDate.Day, range.EndDate.Day, range.StartDate.Month, range.StartDate.Year);
+            else if (range.StartDate.Year == range.EndDate.Year)
+                return string.Format("{0}.{1} - {2}.{3}.{4}", range.StartDate.Day, range.StartDate.Month, range.EndDate.Day, range.EndDate.Month, range.EndDate.Year);
+            else
+                return string.Format("{0}.{1}.{2} - {3}.{4}.{5}", range.StartDate.Day, range.StartDate.Month, range.StartDate.Year, range.EndDate.Day, range.EndDate.Month, range.EndDate.Year);
+        }
 
     }
-
 }
