b02f28198559dddec3895ed6d22d6c3182684489
Zbigniew Tokarski <zbigniew.piotr.tokarski@gmail.com>
2017-11-09 13:56:48 +0100

    Test TryParseToLocalDate method

---
+++ MW.recruitmentTests/StringExtensionMethodsTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MW.recruitment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MW.recruitment.Tests
+{
+    [TestClass()]
+    public class StringExtensionMethodsTests
+    {
+        [TestMethod()]
+        public void TryParseToLocalDate_CorrectParam_GetTrue()
+        {
+            //var testString = new DateTime(01,01,01).ToShortDateString();
+            var testString = "2001/01/01";
+            Assert.IsTrue(testString.TryParseToLocalDate(out DateTime result));
+        }
+
+        [TestMethod()]
+        public void TryParseToLocalDate_MonthNameParam_GetTrue()
+        {
+            //var testString = new DateTime(01,01,01).ToShortDateString();
+            var testString = "01/Jan/01";
+            Assert.IsTrue(testString.TryParseToLocalDate(out DateTime result));
+        }
+
+        [TestMethod()]
+        public void TryParseToLocalDate_ShortYearParam_GetTrue()
+        {
+            //var testString = new DateTime(01,01,01).ToShortDateString();
+            var testString = "01/01/01";
+            Assert.IsTrue(testString.TryParseToLocalDate(out DateTime result));
+        }
+
+        [TestMethod()]
+        public void TryParseToLocalDate_DifferentSeparatorsParam_GetTrue()
+        {
+            //var testString = new DateTime(01,01,01).ToShortDateString();
+            var testString = "01:01:01";
+            Assert.IsTrue(testString.TryParseToLocalDate(out DateTime result));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DateFormatException),"Wrong date format.")]
+        public void TryParseToLocalDate_IncorrectParam_ThrowsDateFormatException()
+        {
+            var testString = "asdf";
+            testString.TryParseToLocalDate(out DateTime result);
+        }
+    }
+}
